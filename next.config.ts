2a938197475783b6fd9d8fee18d41745ae8f8a28
EDUARDO LOZANO CORTES <EDUARINAM@gmail.com>
2025-07-01 04:31:00 +0000

    Puesdes cachearlo o hacer algo para que funcione sin internet aunque se

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,11 @@
 import type {NextConfig} from 'next';
+import withPWA from '@ducanh2912/next-pwa';
+
+const pwaConfig = withPWA({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+});
 
 const nextConfig: NextConfig = {
   /* config options here */
@@ -20,4 +27,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default nextConfig;
+export default pwaConfig(nextConfig);
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,8 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta name="theme-color" content="#222222" />
+        <link rel="manifest" href="/manifest.json" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
